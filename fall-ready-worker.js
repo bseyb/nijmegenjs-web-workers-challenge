4373c710683d1ce15da65eb9aa3ee96bf09fd564
Barry Nijenhuis <barry.nijenhuis@gmail.com>
2014-10-24 21:52:35 +0200

    first version

--- fall-ready-worker.js
+++ fall-ready-worker.js
@@ -1,3 +1,22 @@
-var FallReadyWorker = function(){
+var FallReadyWorker = function(obj){
 
+	this.worker = obj.worker;
+	this.onMessage = obj.onMessage; 
+	
+	this.say = function(param){
+
+		if(typeof Worker != 'function'){
+			var e = {};
+			e.data = this.worker.func(param);
+			this.onMessage(e);
+		}
+		else{
+			var workerstring = 'var func = '+String(this.worker.func)+';'+"self.addEventListener('message', function(e){self.postMessage("+String(this.worker.getPostData)+"(e));}, false);";
+			var funcblob = new Blob([workerstring]);
+			var funcurl = window.URL.createObjectURL(funcblob);
+			this.workerinstance = new Worker(funcurl);
+			this.workerinstance.addEventListener('message', this.onMessage, false);
+			this.workerinstance.postMessage(param);
+		}
+	};
 };
