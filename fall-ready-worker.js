fa6d21f5f8c361fd6a6ad7c7942fb7cff9b406cc
Barry Nijenhuis <barry.nijenhuis@gmail.com>
2014-10-24 22:01:55 +0200

    gold version

--- fall-ready-worker.js
+++ fall-ready-worker.js
@@ -1,22 +1,31 @@
-var FallReadyWorker = function(obj){
+if(typeof Worker != 'function'){
+	var FallReadyWorker = function(obj){
 
-	this.worker = obj.worker;
-	this.onMessage = obj.onMessage; 
-	
-	this.say = function(param){
+		this.worker = obj.worker;
+		this.onMessage = obj.onMessage;
 
-		if(typeof Worker != 'function'){
+		this.say = function(param){
 			var e = {};
 			e.data = this.worker.func(param);
 			this.onMessage(e);
-		}
-		else{
-			var workerstring = 'var func = '+String(this.worker.func)+';'+"self.addEventListener('message', function(e){self.postMessage("+String(this.worker.getPostData)+"(e));}, false);";
-			var funcblob = new Blob([workerstring]);
-			var funcurl = window.URL.createObjectURL(funcblob);
-			this.workerinstance = new Worker(funcurl);
-			this.workerinstance.addEventListener('message', this.onMessage, false);
-			this.workerinstance.postMessage(param);
-		}
+		};
 	};
-};
+
+}
+else{
+	var FallReadyWorker = function(obj){
+
+		this.worker = obj.worker;
+		this.onMessage = obj.onMessage; 
+			
+		var workerstring = 'var func = '+String(this.worker.func)+';'+"self.addEventListener('message', function(e){self.postMessage("+String(this.worker.getPostData)+"(e));}, false);";
+		var funcblob = new Blob([workerstring]);
+		var funcurl = window.URL.createObjectURL(funcblob);
+		this.workerinstance = new Worker(funcurl);
+		this.workerinstance.addEventListener('message', this.onMessage, false);
+
+		this.say = function(param){
+			this.workerinstance.postMessage(param);
+		};
+	};
+}
